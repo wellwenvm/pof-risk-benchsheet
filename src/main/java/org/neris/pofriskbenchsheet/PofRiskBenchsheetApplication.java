1740de3d5dc6006af79bc8283bf11a460b028ed6
sijiyueyueniaofei <w@W.com>
2021-06-04 09:28:55 +0800

    Initial commit

---
+++ src/main/java/org/neris/pofriskbenchsheet/PofRiskBenchsheetApplication.java
@@ -0,0 +1,13 @@
+package org.neris.pofriskbenchsheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PofRiskBenchsheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PofRiskBenchsheetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/neris/pofriskbenchsheet/PofRiskBenchsheetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neris.pofriskbenchsheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PofRiskBenchsheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
